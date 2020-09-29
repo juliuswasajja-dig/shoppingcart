5e1c1863e218344640da6f74493913d07ba2c942
juliuswasajja-dig <65942820+juliuswasajja-dig@users.noreply.github.com>
2020-09-29 07:41:07 +0300

    Initial setup

---
+++ cart.js
@@ -0,0 +1,33 @@
+//console.log("julius wasajja");
+var items = ["soap", "sugar", "kettle", "bedsheet", "soda", "milk"];
+
+//display items in the car
+for (let i = 0; i < items.length; i++) {
+  console.log(items[i]);
+}
+
+//console.log(items[4]);
+items.pop(); //removes the last added item
+items.push("motorola"); // adds item into cart (bottm/last)
+items.unshift("kabiriti"); // adds item at into cart (top)
+//items[0] = "laptops";// replaces "item" in index 0 position
+
+// phones.push(phones[3] = "lg", "panasonic");
+
+// console.log(phones);
+
+/* var phones = ["tecno", "iphone", "nokia", "samsung", "google_pixel", "itel"];
+
+for (let item = 0; item <= phones.length; item++) {
+
+    console.log(phones[item]);
+}
+
+//another way of looping through the array
+
+for (let item of phones) {
+
+    console.log(item);
+
+
+}*/
